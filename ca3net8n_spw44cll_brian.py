1f75c20f8cf1056cc62cce80f8e3d2426d87f182
Szentes Kunigunda <sz.kunigunda@gmail.com>
2017-04-25 13:03:34 +0200

    Update ca3net8n_spw44cll_brian.py

--- ca3net8n_spw44cll_brian.py
+++ ca3net8n_spw44cll_brian.py
@@ -12,41 +12,39 @@ May 2006
 $Id: brunel.py 705 2010-02-03 16:06:52Z apdavison $
 
 """
-
+#31069635088
 from pyNN.utility import get_script_args, Timer
 import numpy as np
 import matplotlib.pyplot as plt
 simulator_name = "brian"
 exec("from %s import *" %simulator_name)
-#simulator_name = get_script_args(1)[0]  
-#exec("from pyNN.%s import *" % simulator_name)
-#from sIN_4pyNN8e import sIN3_Type
-from pyNN.random import NumpyRNG, RandomDistribution
+#from pyNN.random import NumpyRNG, RandomDistribution
+from detect_oscillations_15_50 import replay, ripple, gamma
+
 
 # seed for random generator(s) used during simulation
 kernelseed  = 43210987      
 
 timer = Timer()
-
 run_name = "ca3net8n_spw44cll_brian"
 
+
+X = np.zeros((19,1))
 dt      = 0.1    # the resolution in ms
-simtime = 200000.0 # Simulation time in ms
+simtime = 1000.0 # Simulation time in ms (ideally 10s)
 maxdelay   = 4.0    # synaptic delay in ms
 
 epsilon_Pyr = 0.1  # connection probability
 epsilon_Bas = 0.25
 epsilon_Sli = 0.1
 
-N_Pyr_all = 400 #7200
-N_Bas =50 #400
-N_Sli =50 #400
+N_Pyr_all = 7200 # number of cells
+N_Bas =400
+N_Sli = 400
 N_neurons = N_Pyr_all + N_Bas + N_Sli
 
 NP = 10   # number of excitatory subpopulations
 N_Pyr = int(N_Pyr_all / NP) # number of pyramidal cells in each subpopulation
-
-# N_rec      = 50 # record from 50 neurons
 
 C_Exc  = int(epsilon_Pyr*N_Pyr_all) # number of excitatory synapses per neuron
 C_Inh  = int(epsilon_Bas*N_Bas) # number of fast inhibitory synapses per neuron
@@ -58,7 +56,7 @@ N_syn = (C_Exc+1)*N_neurons + C_Inh*(N_Pyr+N_Bas) + C_SlowInh*N_Pyr
 
 # Initialize the parameters of the integrate and fire neurons
 
-z = 1*nS #?
+z = 1*uS # it gives unit to synaptic conductances (g_) #nS
 gL_Pyr = 3.3333*uS
 tauMem_Pyr = 60.0*ms
 Cm_Pyr = tauMem_Pyr * gL_Pyr
@@ -68,8 +66,8 @@ theta_Pyr = -50.0*mV
 tref_Pyr = 5.0*ms
 
 # Adaptation parameters for pyr cells
-a_Pyr = -0.8*nS  # nS    Subthreshold adaptation conductance
-b_Pyr = 0.04*nA  # nA    Spike-triggered adaptation
+a_Pyr = -0.8*nS  # Subthreshold adaptation conductance
+b_Pyr = 0.04*nA  # Spike-triggered adaptation
 delta_T_Pyr = 2.0*mV  # Slope factor
 tau_w_Pyr = 300*ms  # Adaptation time constant
 v_spike_Pyr = theta_Pyr + 10 * delta_T_Pyr
@@ -94,16 +92,16 @@ tref_Sli = 2.0*ms
 J_PyrExc  = 0.15
 J_PyrInh  = 4.0
 J_BasExc  = 1.5
-J_BasInh  = 2.0
-J_PyrSlowInh = 3.0
-J_SliExc = 0.1
+J_BasInh  = 6.0 # 2.0
+J_PyrSlowInh = 5.0 #3.0 # slow->pyr
+J_SliExc = 0.1 #0.1 # pyr->slow
 J_PyrExt = 0.3
 J_PyrMF = 5.0
 J_BasExt = 0.3
 J_SliExt = 0.3
 
 # Potentiated synapses
-dpot = 10.0       # degree of potentiation
+dpot = 10.0 #10.0       # degree of potentiation
 J_PyrPot = (1 + dpot) * J_PyrExc
 
 # Synaptic reversal potentials
@@ -116,7 +114,9 @@ tauSyn_PyrInh = 3.0*ms
 tauSyn_BasExc = 3.0*ms
 tauSyn_BasInh = 1.5*ms
 tauSyn_SliExc = 5.0*ms
+tauSyn_SliInh = 3.0*ms
 tauSyn_PyrSlowInh = 30.0*ms 
+
 
 # Synaptic delays
 delay_PyrExc = 3.0*ms
@@ -126,20 +126,19 @@ delay_BasInh = 1.5*ms
 delay_SliExc = 3.0*ms
 delay_PyrSlowInh = 2.0*ms
 
-p_rate_pyr = 1.0e-8*Hz
-p_rate_mf = 0.8*Hz      # 3.0
-p_rate_bas = 1.0e-8*Hz
-p_rate_sli = 1.0e-8*Hz
-
-
+p_rate_pyr = 0.0*Hz  
+p_rate_mf = 3.0*Hz #3.0 #0.8
+p_rate_bas = 0.0*Hz  
+p_rate_sli = 0.0*Hz 
 
 # cell equations
 
 eqs_adexp = '''
-dvm/dt = (-gL_Pyr*(vm-Vrest_Pyr) + gL_Pyr*delta_T_Pyr*exp((vm- theta_Pyr)/delta_T_Pyr)-w - (g_ampa*z*(vm-E_Exc) + g_gaba*z*(vm-E_Inh)))/Cm_Pyr : volt
+dvm/dt = (-gL_Pyr*(vm-Vrest_Pyr) + gL_Pyr*delta_T_Pyr*exp((vm- theta_Pyr)/delta_T_Pyr)-w - (g_ampa*z*(vm-E_Exc) + g_gaba_fast*z*(vm-E_Inh)+ g_gaba_slow*z*(vm-E_Inh)))/Cm_Pyr : volt
 dw/dt = (a_Pyr*(vm- Vrest_Pyr )-w)/tau_w_Pyr : amp
 dg_ampa/dt = -g_ampa/tauSyn_PyrExc : 1
-dg_gaba/dt = -g_gaba/tauSyn_PyrInh : 1
+dg_gaba_fast/dt = -g_gaba_fast/tauSyn_PyrInh : 1
+dg_gaba_slow/dt = -g_gaba_slow/tauSyn_PyrSlowInh : 1
 '''
 
 reset_adexp = '''
@@ -156,7 +155,7 @@ dg_gaba/dt = -g_gaba/tauSyn_BasInh : 1
 eqs_sli = '''
 dvm/dt = (-gL_Sli*(vm-Vrest_Sli) - (g_ampa*z*(vm-E_Exc) + g_gaba*z*(vm-E_Inh)))/Cm_Sli :volt
 dg_ampa/dt = -g_ampa/tauSyn_SliExc : 1
-dg_gaba/dt = -g_gaba/tauSyn_PyrSlowInh : 1
+dg_gaba/dt = -g_gaba/tauSyn_SliInh : 1
 '''
 
 def myresetfunc(P, spikes):
@@ -167,45 +166,24 @@ def myresetfunc(P, spikes):
 SCR = SimpleCustomRefractoriness(myresetfunc, tref_Pyr, state='vm')
 
 # === Build the network ========================================================
-
-# clear all existing network elements and set resolution and limits on delays.
-# For NEST, limits must be set BEFORE connecting any elements
-
-##extra = {'threads' : 2}
-#extra = {}
-
-#rank = setup(timestep=dt, max_delay=maxdelay, **extra)
-#num = num_processes()
-#import socket
-#host_name = socket.gethostname()
-#print "Host #%d is on %s" % (rank+1, host_name)
-
-#if extra.has_key('threads'):
-#    print "%d Initialising the simulator with %d threads..." %(rank, extra['threads'])
-#else:
-#    print "%d Initialising the simulator with single thread..." %(rank)
-
-## Small function to display information only on node 1
-#def nprint(s):
-#    if (rank == 0):
-#        print s
-
-#timer.start() # start timer on construction    
-
 #print "%d Setting up random number generator" %rank
 #rng = NumpyRNG(kernelseed)
 
-#vrest_sd = 5
-#vrest_distr = RandomDistribution('uniform', [Vrest_Pyr - np.sqrt(3)*vrest_sd, Vrest_Pyr + np.sqrt(3)*vrest_sd], rng)
 
 Pyr_net = []
+vrest_sd = 5*mV
+upper = Vrest_Pyr + np.sqrt(3)*vrest_sd
+lower = Vrest_Pyr - np.sqrt(3)*vrest_sd
 
 for sp in range(NP):
     print "Creating pyramidal cell population %d  with %d neurons." % (sp, N_Pyr)
     Pyr_subnet = NeuronGroup(N_Pyr, model=eqs_adexp, threshold=v_spike_Pyr, reset=SCR)
-    Pyr_subnet.vm = Vrest_Pyr
+    Pyr_subnet.Vrest_Pyr = rand(N_Pyr)*(upper-lower) + lower # changing pyramidal resting potential to random values
+    Pyr_subnet.w = 0
     Pyr_subnet.g_ampa = 0
-    Pyr_subnet.g_gaba = 0
+    Pyr_subnet.g_gaba_fast = 0
+    Pyr_subnet.g_gaba_slow = 0
+    Pyr_subnet.vm = Vrest_Pyr
 
     Pyr_net.append(Pyr_subnet)
 
@@ -219,8 +197,6 @@ Bas_net.g_gaba = 0
 
 print "Creating slow inhibitory population with %d neurons." % (N_Sli)
 Sli_net = NeuronGroup(N_Sli, model=eqs_sli, threshold=theta_Sli, reset=reset_Sli, refractory=tref_Sli)
-
-
 
 print "Creating external inputs"
 Pyr_input = []
@@ -237,30 +213,12 @@ for sp in range(NP):
     Pyr_input_MF.append(MF)
 
 
-
 print "Creating basket cell external input with rate %g spikes/s." % (p_rate_bas)
 BP = PoissonGroup(N_Bas, p_rate_bas)
 
 print "Creating slow inhibitory cell external input with rate %g spikes/s." % (p_rate_sli)
 PS = PoissonGroup(N_Sli, p_rate_sli) 
-
-
-# Creating external input - is this correct on multiple threads?
-
-## Record spikes
-#for sp in range(NP):
-#    print "%d Setting up recording in excitatory population %d." % (rank, sp)
-#    Pyr_net[sp].record()
-#    Pyr_net[sp][[0, 1]].record_v()
-
-#print "%d Setting up recording in basket cell population." % rank
-#Bas_net.record()
-#Bas_net[[0, 1]].record_v()
-
-#print "%d Setting up recording in slow inhibitory population." % rank
-#Sli_net.record()
-#Sli_net[[0, 1]].record_v()
-
+ 
 print 'Connecting the network'
 
 Pyr_to_Pyr = []
@@ -275,64 +233,53 @@ for post in range(NP):
     for pre in range(NP):
 	print "Connecting pyramidal cell subpopulations %d and %d." % (pre, post)
 	if pre == post:
-	    P2P_sub = Connection(Pyr_net[pre], Pyr_net[post], 'g_ampa',weight=J_PyrPot, sparsness=epsilon_Pyr,delay=delay_PyrExc)
+	    P2P_sub = Connection(Pyr_net[pre], Pyr_net[post], 'g_ampa',weight=J_PyrPot, sparseness=epsilon_Pyr,delay=delay_PyrExc)
 	else:
-	    P2P_sub = Connection(Pyr_net[pre], Pyr_net[post], 'g_ampa', weight=J_PyrExc, sparsness=epsilon_Pyr,delay=delay_PyrExc)
-	print "Pyr --> Pyr\t\t"#, len(P2P_sub), "connections"
+	    P2P_sub = Connection(Pyr_net[pre], Pyr_net[post], 'g_ampa', weight=J_PyrExc, sparseness=epsilon_Pyr,delay=delay_PyrExc)
+	print "Pyr --> Pyr\t\t"
 	Pyr_to_Pyr[post].append(P2P_sub)
-    B2P_sub = Connection(Bas_net, Pyr_net[post], 'g_gaba', weight=J_PyrInh, sparsness=epsilon_Bas,delay=delay_PyrInh)
-    print "Bas --> Pyr\t\t"#, len(B2P_sub), "connections"
+    B2P_sub = Connection(Bas_net, Pyr_net[post], 'g_gaba_fast', weight=J_PyrInh, sparseness=epsilon_Bas,delay=delay_PyrInh)
+    print "Bas --> Pyr\t\t"
     Bas_to_Pyr.append(B2P_sub)
-    S2P_sub = Connection(Sli_net, Pyr_net[post], 'g_ampa', weight=J_PyrSlowInh, sparsness=epsilon_Sli,delay=delay_PyrSlowInh) # g_ampa??
-    print "SlowInh --> Pyr\t\t"#, len(S2P_sub), "connections"
+    S2P_sub = Connection(Sli_net, Pyr_net[post], 'g_gaba_slow', weight=J_PyrSlowInh, sparseness=epsilon_Sli,delay=delay_PyrSlowInh)
+    print "SlowInh --> Pyr\t\t"
     Sli_to_Pyr.append(S2P_sub)
-    E2P_sub = Connection(Pyr_input[post], Pyr_net[post], 'g_ampa', weight=J_PyrExt,delay=dt)
-    E2P_sub.connect_one_to_one(Pyr_input[post],Pyr_net[post]) #?
-    print "Ext --> Pyr\t"#, len(E2P_sub), "connections"
+    E2P_sub = IdentityConnection(Pyr_input[post], Pyr_net[post], 'g_ampa', weight=J_PyrExt)
+    print "Ext --> Pyr\t"
     Ext_to_Pyr.append(E2P_sub)
-    MF2P_sub = Connection(Pyr_input_MF[post], Pyr_net[post], 'g_ampa',weight=J_PyrMF, delay=dt)
-    MF2P_sub.connect_one_to_one(Pyr_input_MF[post], Pyr_net[post])
-    print "MF --> Pyr\t"#, len(MF2P_sub), "connections"
-    MF_to_Pyr.append(MF2P_sub) #? elmenteni melyiket kell??
+    MF2P_sub = IdentityConnection(Pyr_input_MF[post], Pyr_net[post], 'g_ampa',weight=J_PyrMF)
+    print "MF --> Pyr\t"
+    MF_to_Pyr.append(MF2P_sub)
 
 Pyr_to_Bas = []
 
 print "Connecting basket cell population."
 for pre in range(NP):
-    P2B_sub = Connection(Pyr_net[pre], Bas_net, 'g_ampa', weight=J_BasExc,sparsness=epsilon_Pyr, delay=delay_BasExc)
-    print "Pyr --> Bas\t\t"#, len(P2B_sub), "connections"
+    P2B_sub = Connection(Pyr_net[pre], Bas_net, 'g_ampa', weight=J_BasExc,sparseness=epsilon_Pyr, delay=delay_BasExc)
+    print "Pyr --> Bas\t\t"
     Pyr_to_Bas.append(P2B_sub)
-Bas_to_Bas = Connection(Bas_net, Bas_net, 'g_gaba',weight=J_BasInh,sparsness=epsilon_Bas,delay=delay_BasInh)
-print "Bas --> Bas\t\t"#, len(Bas_to_Bas), "connections"
-Ext_to_Bas = Connection(BP, Bas_net,'g_ampa', weight=J_BasExt,delay=dt)
-Ext_to_Bas.connect_one_to_one(BP, Bas_net)
-print "Ext --> Bas\t"#, len(Ext_to_Bas), "connections"
+Bas_to_Bas = Connection(Bas_net, Bas_net, 'g_gaba',weight=J_BasInh,sparseness=epsilon_Bas,delay=delay_BasInh)
+print "Bas --> Bas\t\t"
+Ext_to_Bas = IdentityConnection(BP, Bas_net,'g_ampa', weight=J_BasExt)
+print "Ext --> Bas\t"
 
 Pyr_to_Sli = []
 
 print "Connecting slow inhibitory population."
 for pre in range(NP):
-    P2S_sub = Connection(Pyr_net[pre], Sli_net, 'g_ampa', weight=J_SliExc,sparsness=epsilon_Pyr, delay=delay_SliExc)
-    print "Pyr --> SlowInh\t\t"#, len(P2S_sub), "connections"
+    P2S_sub = Connection(Pyr_net[pre], Sli_net, 'g_ampa', weight=J_SliExc,sparseness=epsilon_Pyr, delay=delay_SliExc)
+    print "Pyr --> SlowInh\t\t"
     Pyr_to_Sli.append(P2S_sub)
-Ext_to_Sli = Connection(PS, Sli_net, 'g_ampa',weight=J_SliExt, delay=dt)
-Ext_to_Sli.connect_one_to_one(PS, Sli_net)
-print "Ext --> SlowInh\t"#, len(Ext_to_Sli), "connections"
+Ext_to_Sli = IdentityConnection(PS, Sli_net, 'g_ampa',weight=J_SliExt)
+print "Ext --> SlowInh\t"
+
+print 'Connections done'
 
 #read out time used for building
 buildCPUTime = timer.elapsedTime()
-#=== Run simulation ===========================================================
-
-# #run, measure computer time
-#timer.start()  start timer on construction
-#print "Running simulation for %g ms." % (simtime)
-#run(simtime)
-#simCPUTime = timer.elapsedTime()
-
 
 # Monitors
-
-sme = []
+sme = [] # Spike Monitor Exitatory
 for sp in range(NP):
      print "Monitoring excitatory population %d" %sp
      sme.append(SpikeMonitor(Pyr_net[sp]))
@@ -340,12 +287,25 @@ for sp in range(NP):
 print "Monitoring basket cell population."
 smi= SpikeMonitor(Bas_net)
 
-
 print "Monitoring slow inhibitory population." 
 sms=SpikeMonitor(Sli_net)
 
-print "Writing data to file." 
+popre_all = []
+for sp in range(NP):
+    popre_all.append(PopulationRateMonitor(Pyr_net[sp], bin=1*ms))
+popri = PopulationRateMonitor(Bas_net, bin=1*ms)
+poprs= PopulationRateMonitor(Sli_net, bin=1*ms)
 
+#pyr_satemon = StateMonitor(Pyr_net[2], 'vm', record=[0,1])
+#bas_statemon = StateMonitor(Bas_net, 'vm', record=[0,1])
+#sli_statemon = StateMonitor(Sli_net,'vm',record=[0,1,2,3,4,5])
+
+#=== Run simulation ===========================================================
+print "Running simulation for %g ms." % (simtime)
+run(simtime*ms, report='text') 
+
+
+print "Writing data to file." 
 filename_spike_pyr = []
 filename_v_pyr = []
 for sp in range(NP):
@@ -365,14 +325,34 @@ for sp in range(NP):
 np.savez(filename_spike_bas,spikes=smi.spikes,spiketimes=smi.spiketimes.values())
 np.savez(filename_spike_sli,spikes=sms.spikes,spiketimes=sms.spiketimes.values())
 
+# get the average spike rate of piramidals
 
-popre_all = []
+pyr_rate_array=[]
 for sp in range(NP):
-    popre_all.append(PopulationRateMonitor(Pyr_net[sp], bin=1*ms))
-popri = PopulationRateMonitor(Bas_net, bin=1*ms)
-poprs= PopulationRateMonitor(Sli_net, bin=1*ms)
+    pyr_rate_array.append(popre_all[sp].rate)
+
+Pyr_avg_rate = np.mean(pyr_rate_array, axis=0)
+
+# get the average spike rate of baskets
+
+Bas_avg_rate = popri.rate
+
+# detect oscillations
+
+meanEr, rEAC, maxEAC, tMaxEAC, maxEACR, tMaxEACR, fE, PxxE, avgRippleFE, ripplePE = ripple(Pyr_avg_rate, 1000)
+avgGammaFE, gammaPE = gamma(fE, PxxE)
+
+meanIr, rIAC, maxIAC, tMaxIAC, maxIACR, tMaxIACR, fI, PxxI, avgRippleFI, ripplePI = ripple(Bas_avg_rate, 1000)
+avgGammaFI, gammaPI = gamma(fI, PxxI)
 
 
+# saving the results into a matrix
+
+X = [p_rate_mf,
+       meanEr, maxEAC, tMaxEAC, maxEACR, tMaxEACR,
+       meanIr, maxIAC, tMaxIAC, maxIACR, tMaxIACR,
+       avgRippleFE, ripplePE, avgGammaFE, gammaPE,
+       avgRippleFI, ripplePI, avgGammaFI, gammaPI]
 
 # write a short report
 print("\n--- CA3 Network Simulation ---")
@@ -389,10 +369,124 @@ print("Bas -> Pyr weight  : %g" % J_PyrInh)
 print("Bas -> Bas weight  : %g" % J_BasInh)
 print("Pyr -> SlowInh weight  : %g" % J_SliExc)
 print("SlowInh -> Pyr weight  : %g" % J_PyrSlowInh)
-#print("Pyramidal cell rate    : %g Hz" % Pyr_rate)
-#print("Basket cell rate    : %g Hz" % Bas_rate)
-#print("Slow inhibitory rate    : %g Hz" % Sli_rate)
-print("Build time         : %g s" % buildCPUTime)   
-#print("Simulation time    : %g s" % simCPUTime)
-  
-# === Clean up and quit ========================================================
+print("Build time         : %g s" % buildCPUTime)
+print("Pyr avg rate: %.2f Hz" % np.mean(Pyr_avg_rate))   
+print("Bas avg rate: %.2f Hz" % np.mean(popri.rate))
+print("SlowInh avg rate: %.2f Hz" % np.mean(poprs.rate))
+
+# === Plots ========================================================
+
+logic1 = isinstance(PxxE, float) # checks weather it is nan value
+print logic1
+logic2 = isinstance(PxxI, float)
+print logic2
+fig1 = plt.figure(figsize=(14, 20))
+
+if not(logic1):
+	ax = fig1.add_subplot(4, 1, 1)
+	ax.plot(np.linspace(0, 1000, len(Pyr_avg_rate)), Pyr_avg_rate, 'b-')
+	ax.set_title('Pyramidal population rate | Mf exitation = %.2f Hz' % p_rate_mf)
+	ax.set_xlabel('Time [ms]')
+	ax.set_ylabel('Pyr. rate [Hz]')
+
+	PxxEPlot = 10 * np.log10(PxxE / max(PxxE))
+
+	fE = np.asarray(fE)
+	rippleS = np.where(145 < fE)[0][0]
+	rippleE = np.where(fE < 250)[0][-1]
+	gammaS = np.where(15 < fE)[0][0]
+	gammaE = np.where(fE < 50)[0][-1]
+	fE.tolist()
+
+	PxxRipple = PxxE[rippleS:rippleE]
+	PxxGamma = PxxE[gammaS:gammaE]
+
+	fRipple = fE[rippleS:rippleE]
+	fGamma = fE[gammaS:gammaE]
+
+	PxxRipplePlot = 10 * np.log10(PxxRipple / max(PxxE))
+	PxxGammaPlot = 10 * np.log10(PxxGamma / max(PxxE))
+
+	ax1 = fig1.add_subplot(4, 1, 2)
+	ax1.plot(fE, PxxEPlot, 'b-', marker='o', linewidth=1.5)
+	ax1.plot(fRipple, PxxRipplePlot, 'r-', marker='o', linewidth=2)
+	ax1.plot(fGamma, PxxGammaPlot, 'k-', marker='o', linewidth=2)
+	ax1.set_title('Power Spectrum Density')
+	ax1.set_xlim([0, 500])
+	ax1.set_xlabel('Frequency [Hz]')
+	ax1.set_ylabel('PSD [dB]')
+	
+	ax6 = fig1.add_subplot(4, 1, 3)
+	ax6.plot(np.linspace(0, 1000, len(Bas_avg_rate)), Bas_avg_rate, 'b-')
+	ax6.set_title('Bas. population rate | Mf exitation = %.2f Hz' % p_rate_mf)
+	ax6.set_xlabel('Time [ms]')
+	ax6.set_ylabel('Bas. rate [Hz]')
+
+if not(logic2):
+	PxxIPlot = 10 * np.log10(PxxI / max(PxxI))
+
+	fI = np.asarray(fI)
+	rippleS = np.where(145 < fI)[0][0]
+	rippleE = np.where(fI < 250)[0][-1]
+	gammaS = np.where(15 < fI)[0][0]
+	gammaE = np.where(fI < 50)[0][-1]
+	fI.tolist()
+
+	PxxRipple = PxxI[rippleS:rippleE]
+	PxxGamma = PxxI[gammaS:gammaE]
+
+	fRipple = fI[rippleS:rippleE]
+	fGamma = fI[gammaS:gammaE]
+
+	PxxRipplePlot = 10 * np.log10(PxxRipple / max(PxxI))
+	PxxGammaPlot = 10 * np.log10(PxxGamma / max(PxxI))
+
+	ax7 = fig1.add_subplot(4, 1, 4)
+	ax7.plot(fI, PxxIPlot, 'b-', marker='o', linewidth=1.5)
+	ax7.plot(fRipple, PxxRipplePlot, 'r-', marker='o', linewidth=2)
+	ax7.plot(fGamma, PxxGammaPlot, 'k-', marker='o', linewidth=2)
+	ax7.set_title('Power Spectrum Density')
+	ax7.set_xlim([0, 500])
+	ax7.set_xlabel('Frequency [Hz]')
+	ax7.set_ylabel('PSD [dB]')
+
+fig1.tight_layout()
+fig1.savefig('gammaPSD%.2fmf_%.2fslowpyr_%.2fpyrslow_%ddpot_%.2fjbasinh.png' % (p_rate_mf,J_PyrSlowInh,J_SliExc,dpot,J_BasInh))
+
+
+fig2 = plt.figure(figsize=(10, 8))
+
+ax = fig2.add_subplot(3,1,1)
+ax.plot(np.linspace(0, simtime, len(Pyr_avg_rate)), Pyr_avg_rate, 'g-')
+ax.set_title('Pyr. pop avg rate | Mf exitation = %.2f Hz' % p_rate_mf)
+
+ax = fig2.add_subplot(3,1,2)
+ax.plot(np.linspace(0, simtime, len(popri.rate)), popri.rate, 'g-')
+ax.set_title('Bas. population rate')
+
+ax = fig2.add_subplot(3,1,3)
+ax.plot(np.linspace(0, simtime, len(poprs.rate)), poprs.rate, 'g-')
+ax.set_title('Slow inh. population rate')
+ax.set_xlabel('Time [ms]')
+
+fig2.savefig('avg_rate%.2fmf_%.2fslowpyr_%.2fpyrslow_%ddpot_%.2fjbasinh.png' % (p_rate_mf,J_PyrSlowInh,J_SliExc,dpot,J_BasInh))
+
+fig3 = plt.figure(figsize=(10, 8))
+
+ax = fig3.add_subplot(3,1,1)
+ax.plot(np.linspace(900, 1000, len(Pyr_avg_rate[900:1000])), Pyr_avg_rate[900:1000], 'g-', linewidth=1.5)
+ax.set_title('Pyr. pop avg rate | Mf exitation = %.2f Hz' % p_rate_mf)
+
+ax = fig3.add_subplot(3,1,2)
+ax.plot(np.linspace(900, 1000, len(popri.rate[900:1000])), popri.rate[900:1000], 'g-', linewidth=1.5)
+ax.set_title('Bas. population rate')
+
+ax = fig3.add_subplot(3,1,3)
+ax.plot(np.linspace(900, 1000, len(poprs.rate[900:1000])), poprs.rate[900:1000], 'g-', linewidth=1.5)
+ax.set_title('Slow inh. population rate')
+ax.set_xlabel('Time [ms]')
+
+fig3.savefig('avg_rate_last100_%.2fmf_%.2fslowpyr_%.2fpyrslow_%ddpot_%.2fjbasinh.png' % (p_rate_mf,J_PyrSlowInh,J_SliExc,dpot,J_BasInh))
+
+
+plt.close('all')
